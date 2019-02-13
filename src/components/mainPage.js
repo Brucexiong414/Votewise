9d4c4bccb489ac1d2a611cd3a2d3b34294d2bf0b
yiranchen1 <chenyiran2016@gmail.com>
2019-02-13 15:17:14 -0600

    make buttons responsive

--- src/components/mainPage.js
+++ src/components/mainPage.js
@@ -1,4 +1,5 @@
 import React, {Component} from "react"
+import {Row, Col} from "react-bootstrap"
 import FoodCard from "./FoodCard";
 import MovieCard from "./MovieCard";
 import "./mainPage.css"
@@ -26,10 +27,20 @@ class MainPage extends React.Component {
   render() {
     return (<div>
       <div className="buttonGroups">
-        <button className="generalButton foodButton" onClick={(e) => this.handleClick("food", e)}>Food</button>
-        <button className="generalButton" onClick={(e) => this.handleClick("meeting", e)}>Meeting</button>
-        <button className="generalButton" onClick={(e) => this.handleClick("movie", e)}>Movie</button>
-        <button className="generalButton" onClick={(e) => this.handleClick("time", e)}>Time</button>
+        <Row>
+          <Col sm={3}>
+            <button className="generalButton foodButton" onClick={(e) => this.handleClick("food", e)}>Food</button>
+          </Col>
+          <Col sm={3}>
+            <button className="generalButton" onClick={(e) => this.handleClick("meeting", e)}>Meeting</button>
+          </Col>
+          <Col sm={3}>
+            <button className="generalButton" onClick={(e) => this.handleClick("movie", e)}>Movie</button>
+          </Col>
+          <Col sm={3}>
+            <button className="generalButton" onClick={(e) => this.handleClick("time", e)}>Time</button>
+          </Col>
+        </Row>
       </div>
       {this.renderCard()}
     </div>)
