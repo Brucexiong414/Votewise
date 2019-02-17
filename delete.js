c9e9c201133dd11d1ebfc4ef2545c29b89b19949
Yunxi Xiong <xiong@Yunxis-MacBook-Pro.local>
2019-02-16 22:57:30 -0600

    backend experiment

---
+++ delete.js
@@ -0,0 +1,20 @@
+import * as dynamoDbLib from "./libs/dynamodb-lib";
+import { success, failure } from "./libs/response-lib";
+
+export async function main(event, context) {
+    const params = {
+        TableName: "notes",
+        // 'Key' defines the partition key and sort key of the item to be removed
+        // - 'userId': Identity Pool identity id of the authenticated user
+        Key: {
+            userId: event.requestContext.identity.cognitoIdentityId,
+        }
+    };
+
+    try {
+        const result = await dynamoDbLib.call("delete", params);
+        return success({ status: true });
+    } catch (e) {
+        return failure({ status: false });
+    }
+}
