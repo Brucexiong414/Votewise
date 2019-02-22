045e20b04ffc3ce02447f225b7616e117672f4a0
Yunxi Xiong <xiong@Yunxis-MacBook-Pro.local>
2019-02-21 22:18:07 -0600

    change user pool

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 export default {
     cognito: {
-        REGION: "us-west-2",
-        USER_POOL_ID: "us-west-2_UJHt4Xwh3",
-        APP_CLIENT_ID: "4u9vqj00vl8qhvmossfj01absu",
+        REGION: "us-east-1",
+        USER_POOL_ID: "us-east-1_j4sdcsFHj",
+        APP_CLIENT_ID: "7ms7bfu87mo4e5nl77djiu6tpk",
     }
 };
