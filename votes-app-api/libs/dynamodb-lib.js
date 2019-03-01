971c217d3d2213a83bb6f177d1aa5aac9358c45c
Yunxi Xiong <xiong@Yunxis-MacBook-Pro.local>
2019-03-01 01:57:28 -0600

    continue work on backend and db

--- votes-app-api/libs/dynamodb-lib.js
+++
@@ -1,8 +0,0 @@
-import AWS from "aws-sdk";
-
-export function call(action, params) {
-  AWS.config.update({ region: "us-west-2" });
-  const dynamoDb = new AWS.DynamoDB.DocumentClient();
-
-  return dynamoDb[action](params).promise();
-}
